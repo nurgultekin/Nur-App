93e80d9f05889aeea0ae2fc1a5cf65b7bf815f36
nurgultekin <92712594+nurgultekin@users.noreply.github.com>
2024-07-11 00:46:04 +0200

    tailwind styling LuxeBite

--- nur-website/src/assets/components/CaseStudy1.jsx
+++ nur-website/src/assets/components/CaseStudy1.jsx
@@ -10,16 +10,16 @@ const CaseStudy1 = () => {
     <div className="flex flex-col items-center justify-center mt-[100px]">
       <AnimationDark></AnimationDark>
 
-      <div className="flex flex-col md:flex-row justify-between mx-[70px] md:mx-[200px]  w-4/5 mt-5">
+      <div className="flex flex-col lg:flex-row justify-between mx-[40px] md:mx-[200px]  w-4/5 mt-5">
 
-        <div className="flex gap-[15px] items-baseline mt-40 ">
+        <div className="flex gap-[15px] items-baseline mt-40 mr-10 max-w-fit">
           <video src={LuxeBiteDesktop} className=" rounded-lg md:block max-w-sm md:max-w-lg sm:max-w-sm" autoPlay loop muted />
           <video src={LuxeBiteMobile} className="rounded-lg md:block max-w-xs md:max-w-md sm:max-w-xsm" autoPlay loop muted />
         </div>
 
-        <div className="text-white flex flex-col justify-center items-start md:mr-[200px] mt-10 md:mt-0">
-          <div className=" font-tomorrow sm:text-[18px] sm:leading-[22px] md:text-[24px] md:leading-[28px] lg:text-[32px] lg:leading-[36px]">Case Study 1: </div>
-          <div className=" font-tomorrow sm:text-[42px] sm:leading-[48px] md:text-[60px] md:leading-[64px] lg:text-[78px] lg:leading-[80px] ">LuxeBite</div>
+        <div className="text-white flex flex-col justify-center items-start md:mr-[200px] mt-10 md:mt-10 mr-10 max-w-fit">
+          <div className=" font-tomorrow text-[18px] sm:text-[18px] sm:leading-[22px] md:text-[24px] md:leading-[28px] lg:text-[32px] lg:leading-[36px]">Case Study 1: </div>
+          <div className=" font-tomorrow text-[42px] sm:text-[42px] sm:leading-[48px] md:text-[60px] md:leading-[64px] lg:text-[78px] lg:leading-[80px] ">LuxeBite</div>
         </div>
       </div>
 
@@ -27,9 +27,9 @@ const CaseStudy1 = () => {
         <div className="flex flex-col items-start gap-[10px]">
           <div className="flex items-center gap-[15px]">
             <div className="w-[21.04px] h-[21.04px] bg-transparent border-[4px] border-lime rounded-full" />
-            <div className="text-[24px] font-tomorrow text-whitesmoke leading-[31.2px]">My role</div>
+            <div className="text-[20px] md:text-[24px] md:leading-[31.2px] font-tomorrow text-whitesmoke">My role</div>
           </div>
-          <div className="text-[18px] font-roboto text-whitesmoke leading-[30px] max-w-[400px] mt-[4px] text-left">
+          <div className="text-[16px] sm:text-[16px] md:text-[18px] lg:text-[20px] font-roboto text-whitesmoke leading-[30px] max-w-[400px] mt-[4px] text-left">
             UX research, UI design, developing design systems, agile project management and branding
           </div>
         </div>
@@ -37,9 +37,9 @@ const CaseStudy1 = () => {
         <div className="flex flex-col items-start gap-[10px]">
           <div className="flex items-center gap-[15px]">
             <div className="w-[21.04px] h-[21.04px] bg-transparent border-[4px] border-lime rounded-full" />
-            <div className="text-[24px] font-tomorrow text-whitesmoke leading-[31.2px]">Scope</div>
+            <div className="text-[20px] md:text-[24px] md:leading-[31.2px] font-tomorrow text-whitesmoke ">Scope</div>
           </div>
-          <div className="text-[18px] font-roboto text-whitesmoke leading-[30px] max-w-[400px] mt-[4px] text-left">
+          <div className="text-[16px] sm:text-[16px] md:text-[18px] lg:text-[20px] font-roboto text-whitesmoke leading-[30px] max-w-[400px] mt-[4px] text-left">
             January 25 - April 5, 2024 -- School project
           </div>
         </div>
@@ -47,9 +47,9 @@ const CaseStudy1 = () => {
         <div className="flex flex-col items-start gap-[10px]">
           <div className="flex items-center gap-[15px]">
             <div className="w-[21.04px] h-[21.04px] bg-transparent border-[4px] border-lime rounded-full" />
-            <div className="text-[24px] font-tomorrow text-whitesmoke leading-[31.2px]">Team</div>
+            <div className="text-[20px] md:text-[24px] md:leading-[31.2px] font-tomorrow text-whitesmoke">Team</div>
           </div>
-          <div className="text-[18px] font-roboto text-whitesmoke leading-[30px] max-w-[400px] mt-[4px] text-left">
+          <div className="text-[16px] sm:text-[16px] md:text-[18px] lg:text-[20px] font-roboto text-whitesmoke leading-[30px] max-w-[400px] mt-[4px] text-left">
             Nur Gültekin, Joakim Strandlund, Niloofar Shahsavar, Julia Kleresjö and Ronja Tiving.
           </div>
         </div>
@@ -57,38 +57,43 @@ const CaseStudy1 = () => {
         <div className="flex flex-col items-start gap-[10px]">
           <div className="flex items-center gap-[15px]">
             <div className="w-[21.04px] h-[21.04px] bg-transparent border-[4px] border-lime rounded-full" />
-            <div className="text-[24px] font-tomorrow text-whitesmoke leading-[31.2px]">Frameworks</div>
+            <div className="text-[20px] md:text-[24px] md:leading-[31.2px] font-tomorrow text-whitesmoke">Frameworks</div>
           </div>
-          <div className="text-[18px] font-roboto text-whitesmoke leading-[30px] max-w-[400px] mt-[4px] text-left">
+          <div className="text-[16px] sm:text-[16px] md:text-[18px] lg:text-[20px] font-roboto text-whitesmoke leading-[30px] max-w-[400px] mt-[4px] text-left">
             UX research, UX Design, UI design with Figma, agile project management
           </div>
         </div>
       </div>
 
-      <div className="text-white flex flex-col items-center w-11/12 md:w-4/5 mx-[100px] md:mx-[200px] my-10">
-        <div className="text-[42px] font-tomorrow text-whitesmoke leading-[42px] mb-5">Project Overview</div>
-        <div className="text-[18px] font-roboto text-whitesmoke leading-[30px] mt-[4px] text-left mx-[30px] md:mx-[200px]">
+
+
+      <div className="text-white flex flex-col items-center w-11/12 md:w-4/5 mx-[100px] md:mx-[200px] mb-5 mt-10">
+        <div className="text-white  font-normal font-['Tomorrow'] text-5xl sm:text-2xl md:text-5xl p-8 mb-5 mt-5 ">PROJECT OVERVIEW</div>
+        <div className="text-[16px] sm:text-[16px] md:text-[18px] lg:text-[20px] font-roboto text-whitesmoke leading-[30px] text-left mx-[100px] md:mx-[200px] lg:max-w-[700px]">
           <p>As part of our vocational education, we were tasked with case study aiming to deliver a distinctive food delivery web shop. My team members and I created a fictional company specializing in delivering exclusive, luxury food using climate-friendly drones, aiming to provide a "golden edge in everyday lives". Our case study showcases extensive market and user experience research, ideation, user interface design, and prototyping of our innovative web shop concept.</p>
         </div>
       </div>
 
-      <div className="text-white grid grid-cols-1 gap-[15px] w-11/12 md:w-4/5 mx-[100px] md:mx-[200px] my-10">
-        <div className="text-[24px] font-tomorrow text-whitesmoke leading-[31.2px]">Problem and Goal</div>
-        <div className="text-[18px] font-roboto text-whitesmoke leading-[30px] text-left">
+      <div className="text-white flex flex-col items-center w-11/12 md:w-4/5 mx-[100px] md:mx-[200px] my-10">
+        <div className="text-[30px] md:text-[36px] lg:text-[42px] font-tomorrow text-whitesmoke mb-5 mt-5">Problem and Goal</div>
+        <div className="text-[16px] sm:text-[16px] md:text-[18px] lg:text-[20px] font-roboto text-whitesmoke leading-[30px] text-left mx-[100px] md:mx-[200px] lg:max-w-[700px] ">
           <p>In the competitive online food delivery market, simplicity and speed are paramount user requirements. Established services already offer diverse options and widespread popularity. To compete in this saturated market, our fictional company must differentiate itself by providing a distinct user experience.</p>
           <p>Through user research, we identified that established food delivery services are often perceived as convenient but lacking in quality and dining experience. Customers desire fine dining experiences even in home deliveries, craving a golden edge in simplicity and ease-of-enjoyment in their everyday lives. Our aim is to fulfill this demand by offering a service that is simple, smooth, unique, and exclusive, setting us apart from traditional delivery options.</p>
         </div>
+      </div>
 
-        <div>
-          <div className="text-[24px] font-tomorrow text-whitesmoke leading-[31.2px]">Solution</div>
-          <div className="text-[18px] font-roboto text-whitesmoke leading-[30px] mt-[4px] text-left">
-            <p>By conducting comprehensive market research, analyzing user behaviors through big data, and engaging in qualitative user research, we pinpointed a growing interest and demand for luxury, exclusive food delivery services that stand out from established brands. In response, we crafted a high-fidelity web shop offering exclusive food. The website design prioritizes simplicity while evoking a sense of exclusivity.</p>
+        <div className="text-white flex flex-col items-center w-11/12 md:w-4/5 mx-[100px] md:mx-[200px] my-10">
+          <div className="text-[30px] md:text-[36px] lg:text-[42px] font-tomorrow text-whitesmoke mb-5 mt-5">Solution</div>
+          <div className="text-[16px] sm:text-[16px] md:text-[18px] lg:text-[20px] font-roboto text-whitesmoke leading-[30px] text-left mx-[100px] md:mx-[200px] lg:max-w-[700px] ">
+            <p>By conducting comprehensive market research, analyzing user behaviors through big data, and engaging in qualitative user research, we pinpointed a growing interest and demand for luxury, exclusive food delivery services that stand out from established brands. </p>
+            <p>In response, we crafted a high-fidelity web shop offering exclusive food. The website design prioritizes simplicity while evoking a sense of exclusivity.</p>
           </div>
         </div>
-      </div>
+
+      
       {/** DESIGN PROCESS SECTION */}
       <div>
-        <div className="text-white text-5xl font-normal font-['Tomorrow'] leading-[62.40px] p-8 mb-9">DESIGN PROCESS
+        <div className="text-white  font-normal font-['Tomorrow'] text-5xl sm:text-2xl md:text-5xl p-8 mb-9 my-10">DESIGN PROCESS
           </div> 
         <div className="w-[1181px] h-[330px] justify-start items-start inline-flex gap-4">
           <div className="flex-col justify-start items-center gap-[9px] inline-flex">

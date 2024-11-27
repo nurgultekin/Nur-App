43af76ba27a12b3f399a1e9337b3fdb37b0ea275
Nur Gültekin <92712594+nurgultekin@users.noreply.github.com>
2024-11-27 02:35:10 +0100

    kappnsCatch modular & clean code

--- nur-website/src/assets/components/caseStudies/KappnsCatch.jsx
+++ nur-website/src/assets/components/caseStudies/KappnsCatch.jsx
@@ -3,33 +3,125 @@ import KappnsCatchMob from '../../media/kappns-catch/mockup-mobile.gif';
 import KappnsCatchDT from '../../media/kappns-catch/mockup-desktop.gif';
 import KappnsCatchDev from '../../media/kappns-catch/codeLove.png';
 import '../../../App.css';
+import Section from "../SectionsLayout";
 import AnimationDark from "../AnimationDark";
 import 'tailwindcss/tailwind.css';
 
 const KappnsCatch = () => {
+
+  /* const overView = [
+    {
+      title: "My role",
+      description: "Lead Frontend Developer",
+    },
+    {
+      title: "Scope",
+      description: "June 2024 (One week) - School project",
+    },
+    {
+      title: "Team",
+      description: "Nur Gültekin, Rasmus Mattsson, Matilda Danielsson, Rebecca Reimers, Kenny Gustafsson",
+    },
+    {
+      title: "Frameworks",
+      description: "React.js, Figma, Tailwind CSS, Scrum",
+    },
+  ]; */
+
+  // Data for other sections
+
+const summary = {
+  title: "Project Overview",
+  paragraph: " As part of a school project, my team and I created Kapp’n’s, a whimsical web shop offering an imaginary selection of living fish products. This playful project combined dynamic content, a smooth checkout process, and an interface styled with Tailwind CSS. The challenge lay in developing a functional and engaging web shop within a one-week timeline, while managing a collaborative workflow to ensure smooth integration of our code. ",
+  content:null,
+  isList:false,
+
+};
+
+const problemAndSolution = {
+  title: "Problem and Solution",
+  paragraph: "Our key challenge was the tight deadline: we had only one week to take Kapp&qrsqo;n&qrsqo;s Catch from concept to completion. As one of the two developers, my role was to manage our codebase and ensure efficient collaboration. Rasmus and I focused on clean, reusable code, utilizing Tailwind CSS to streamline our styling process and speed up interface development with pre-built components. By dynamically fetching product data, we built a website that could adapt to content changes without manual updates, providing a functional shopping experience with dynamic content.",
+  isList: false,
+};
+
+const designAndDevelopmentProcess = {
+  title: "Development Process",
+
+  content: [
+    {
+      title: "Collaborative Code Management",
+      description: "Using GitHub, I took responsibility for managing our branches and merging processes. We adopted a cautious, step-by-step approach to merging, ensuring each update was functional and bug-free before moving changes to the main branch.",
+    },
+    {
+      title: "Dynamic Content",
+      description: "We created a dataset of fictional fish products and used it to populate the site dynamically, which allowed for an evolving product selection. ",
+    },
+    {
+      title: "Time Optimization",
+      description: "Tailwind&qrsqo;s utility-first framework helped us quickly style the interface. By leveraging pre-built components, we saved time without compromising on design quality. This resulted in a visually engaging and cohesive experience.",
+    },
+    {
+      title: "Tools & Frameworks",
+      description: "We used React for building the dynamic elements of the web shop, Tailwind CSS for styling, and Figma for initial design and collaboration with our UX/UI designers. This combination allowed us to work efficiently and achieve a polished product in a short timeframe.",
+    },
+  ],
+  isList: true,
+};
+
+const futureImprovements = {
+  title: "What Could We Improve?",
+
+  content: [
+    {
+      title: "Layout First",
+      description: "Start with the layout, work later on logic and debugging to use the tight deadline even more efficiently.",
+    },
+    {
+      title: "Code Structure",
+      description: "Improve the organization and clarity of the project's code by ensuring a logical structure for component mapping, minimizing code redundancy, and establishing a clean, well-organized file structure.",
+    },
+    {
+      title: "UX & UI",
+      description: "Due to the one-week timeframe, we had limited opportunity to focus on the UX/UI aspects of the project. As a result, we relied on pre-existing styles from the Tailwind library rather than developing a custom user journey map or dedicating time to refining the visual interface. Therefore, the project remains rather simple in terms of design. With more time, we could have placed greater emphasis on enhancing the user experience and visual aesthetics.",
+    },
+  ],
+  isList: true,
+};
+
+const conclusion = {
+  title: "Conclusion",
+  paragraph: "Kapp'n's Catch was a valuable exercise in time management, teamwork, and technical efficiency. Despite the short timeline, our team's commitment to clean code, dynamic functionality, and effective collaboration enabled us to create a functional and delightful product. This experience reinforced the importance of structure and code organization in collaborative projects, and ensured that each member's contributions fit suently into the whole.",
+  content:null,
+  isList:false,
+
+};
+
+
+
+  
   return (
     <div className="min-h-screen flex flex-col justify-between">
       <AnimationDark/>
       
 
-        <div className="flex flex-col lg:flex-row justify-center mx-auto w-4/5 mt-5">
+        <div className="flex flex-col lg:flex-row justify-center mx-auto mt-5">
 
-        <div className="flex gap-[15px] items-baseline mt-40 mr-auto lg:mr-10 max-w-fit object-scale-down mx-auto">
-          <img src={KappnsCatchDT} className=" rounded-lg md:block max-w-sm md:max-w-lg sm:max-w-sm  data-twe-lazy-load-init"  />
-          <img src={KappnsCatchMob} className="rounded-lg hidden lg:block max-w-xs lg:max-w-md  data-twe-lazy-load-init" />
+        <div className="flex gap-[0.5rem] items-center justify-center lg:items-baseline justify-center mt-40  lg:max-w-[40vw] max-w-full object-scale-down mx-auto">
+          <img src={KappnsCatchDT} className=" rounded-lg md:block max-w-[50vw] lg:max-w-[70%] data-twe-lazy-load-init"  />
+          <img src={KappnsCatchMob} className="rounded-lg block lg:block max-w-[30vw] lg:max-w-[40%] data-twe-lazy-load-init" />
         </div>
 
-        <div className="text-white flex flex-col justify-center items-left lg:items-left mt-10 md:mt-10 lg:mt-48 mr-auto lg:mr-10 mx-auto max-w-fit gap-4">
+        <div className="text-white flex flex-col justify-center items-left lg:items-left mt-10 md:mt-10 lg:mt-48 max-w-[70vw] lg:max-w-[40vw] gap-4 mx-auto">
           <div className=" font-['Tomorrow'] text-[18px] sm:text-[18px] sm:leading-[22px] md:text-[24px] md:leading-[28px] lg:text-[32px] lg:leading-[36px]">Case Study 2: </div>
           <div className=" font-['Tomorrow'] text-[42px] sm:text-[42px] sm:leading-[48px] md:text-[60px] md:leading-[64px] lg:text-[78px] lg:leading-[80px] ">Kapp&rsquo;n&rsquo;s Catch</div>
-            <div className="flex gap-4">
-              <a href="https://kappns-catch.netlify.app/" target="_blank"className="primary-button max-w-60 text-black py-2 mt-10 mr-4 ">See the Website 
+            <div className="flex flex-col md:flex-row gap-4 mt-8">
+              <a href="https://kappns-catch.netlify.app/" target="_blank"className="primary-button text-black text-sm py-2 md:mt-10 mr-4 scale-90 md:scale-100 whitespace-nowrap ">See the Website 
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
               </svg>
               </a>
 
-              <a href="https://kappns-catch.netlify.app/" target="_blank"className="secondary-button max-w-60 text-black py-2 mt-10 mr-4 flex gap-2">See on Github 
+              <a href="https://kappns-catch.netlify.app/" target="_blank"className="secondary-button text-black text-sm py-2 md:mt-10 mr-4 flex gap-2 scale-90 md:scale-100 whitespace-nowrap">See on Github 
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M13.5 6H5.25A2.25 2.25 0 0 0 3 8.25v10.5A2.25 2.25 0 0 0 5.25 21h10.5A2.25 2.25 0 0 0 18 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
               </svg>
@@ -38,8 +130,8 @@ const KappnsCatch = () => {
         </div>
         </div>
 
-      <div className="flex flex-col justify-center items-center mx-auto ">
-        <div className="text-white grid grid-cols-1 md:grid-cols-2 gap-12 lg:gap-24 mx-auto md:w-4/5 mt-24 px-3 md:px-4">
+      <div className="flex flex-col justify-center items-center mx-auto mt-24 max-w-[70vw] md:max-w-4/5">
+        <div className="text-white grid grid-cols-1 md:grid-cols-2 gap-12 lg:gap-24 mx-auto mt-24 px-3 md:px-4">
             <div className="flex flex-col items-left gap-[10px]">
             <div className="flex items-center gap-2">
                 <div className="w-[21.04px] h-[21.04px] bg-transparent border-[4px] border-lime-300 rounded-full" />
@@ -83,78 +175,22 @@ const KappnsCatch = () => {
       </div>
 
 
-      <div className="text-white flex flex-col items-center mx-auto mt-48">
-        <div className="text-white  font-normal font-['Tomorrow'] text-center text-3xl md:text-5xl p-8 mb-12 mt-5 border-b border-lime-300 ">Project Overview</div>
-        <div className="text-[16px] sm:text-[16px] md:text-[18px] lg:text-[20px] font-roboto text-whitesmoke leading-[30px] text-left mx-[100px] md:mx-[200px] lg:max-w-[700px]">
-          <p>As part of a school project, my team and I created <span className="italic">Kapp&rsquo;n&rsquo;s</span>, a whimsical web shop offering an imaginary selection of living fish products. This playful project combined dynamic content, a smooth checkout process, and an interface styled with Tailwind CSS. The challenge lay in developing a functional and engaging web shop within a one-week timeline, while managing a collaborative workflow to ensure smooth integration of our code.</p>
-        </div>
-      </div>
+    {/* Summary  */}
+    <Section title={summary.title} paragraph={summary.paragraph}/>
 
-      <div className="text-white flex flex-col items-center justify-center mx-auto mt-48 mb-24 my-10">
-        <div className="text-[30px] md:text-[36px] lg:text-[42px] font-['Tomorrow'] text-whitesmoke mb-12 mt-5 p-8 border-b border-lime-300">Problem and Solution</div>
-        <div className="text-[16px] sm:text-[16px] md:text-[18px] lg:text-[20px] font-roboto text-whitesmoke leading-[30px] text-left mx-[100px] md:mx-[200px] lg:max-w-[700px] ">
-          <p>Our key challenge was the tight deadline: we had only one week to take Kapp&qrsqo;n&qrsqo;s Catch from concept to completion. As one of the two developers, my role was to manage our codebase and ensure efficient collaboration. Rasmus and I focused on clean, reusable code, utilizing Tailwind CSS to streamline our styling process and speed up interface development with pre-built components. By dynamically fetching product data, we built a website that could adapt to content changes without manual updates, providing a functional shopping experience with dynamic content.</p>
-        </div>
-      </div>
+    {/* Problem and Solution Section */}
+    <Section title={problemAndSolution.title} paragraph={problemAndSolution.paragraph} content={problemAndSolution.content} isList={problemAndSolution.isList} />
 
+    {/* Design and Development Process Section */}
+    <Section title={designAndDevelopmentProcess.title} paragraph={designAndDevelopmentProcess.paragraph} content={designAndDevelopmentProcess.content} isList={designAndDevelopmentProcess.isList} />
 
      
+     {/* Improvements Section */}
+    <Section title={futureImprovements.title} paragraph={futureImprovements.paragraph} content={futureImprovements.content} isList={futureImprovements.isList} />
+     
 
-        <div className="text-white flex flex-col items-center justify-center mx-auto mt-48 my-10">
-          <div className="text-[30px] md:text-[36px] lg:text-5xl font-['Tomorrow'] text-whitesmoke mb-12 mt-5 p-8 border-b border-lime-300" >Development Process</div>
-            <div className="max-w-xl justify-center mx-auto lg:justify-end mb-12">
-                <img src={KappnsCatchDev} alt="Kapp'n's Catch Development Process visualization showing integration of API calls and dynamic web building"></img>
-            </div>
-          <div className="text-[16px] sm:text-[16px] md:text-[18px] lg:text-[20px] font-roboto text-whitesmoke leading-[30px] text-left mx-[100px] md:mx-[200px] lg:max-w-[700px] ">
-            <ul className="flex flex-col gap-8">
-              <li className="list-disc marker:text-lime-300">
-                <span className="text-lime-300">Collaborative Code Management: </span>
-                Using GitHub, I took responsibility for managing our branches and merging processes. We adopted a cautious, step-by-step approach to merging, ensuring each update was functional and bug-free before moving changes to the main branch.</li>
-              <li className="list-disc marker:text-lime-300">
-                <span className="text-lime-300">Dynamic Content: </span>
-                We created a dataset of fictional fish products and used it to populate the site dynamically, which allowed for an evolving product selection. </li>
-              <li className="list-disc marker:text-lime-300">
-                <span className="text-lime-300">Time Optimization: </span>
-                Tailwind&qrsqo;s utility-first framework helped us quickly style the interface. By leveraging pre-built components, we saved time without compromising on design quality. This resulted in a visually engaging and cohesive experience.</li>
-                <li className="list-disc marker:text-lime-300">
-                <span className="text-lime-300">Tools & Frameworks: </span>
-                We used React for building the dynamic elements of the web shop, Tailwind CSS for styling, and Figma for initial design and collaboration with our UX/UI designers. This combination allowed us to work efficiently and achieve a polished product in a short timeframe.
-                </li>
-                </ul>
-          </div>
-        </div>
-
-        <div className="text-white flex flex-col items-center justify-center mx-auto mt-48 my-10">
-          <div className="text-[30px] md:text-[36px] lg:text-[42px] font-['Tomorrow'] text-whitesmoke mb-12 mt-5 p-8 border-b border-lime-300 text-center">
-            What to Improve?
-          </div>
-
-          <ul className="flex flex-col gap-8 text-[16px] sm:text-[16px] md:text-[18px] lg:text-[20px] font-roboto text-whitesmoke leading-[30px] text-left mx-[100px] md:mx-[100px] lg:max-w-[650px]">
-            <li className="list-disc">
-              Start with the layout, work later on logic and debugging to use the tight deadline even more efficiently,
-            </li>
-            <li className="list-disc">
-                Implement a clearer code structure within the project file,
-                </li>
-
-            <li className="list-disc">
-                Allocate more time to UX and UI,
-            </li>
-
-            <li className="list-disc">
-                 Refine Tailwind components for efficiency.
-            </li>
-          </ul>
-        </div>
-
-
-
-        <div className="text-white flex flex-col justify-center items-center mx-auto mt-48 mb-48">
-        <div className="text-white  font-normal font-['Tomorrow'] text-center text-3xl md:text-5xl p-8 mb-5 mt-5 border-b border-lime-300 ">Conclusion</div>
-        <div className="text-[16px] sm:text-[16px] md:text-[18px] lg:text-[20px] font-roboto text-whitesmoke leading-[30px] text-left mx-[100px] md:mx-[200px] lg:max-w-[700px]">
-          <p><span className="italic">Kapp&rsquo;n&rsquo;s</span> Catch was a valuable exercise in time management, teamwork, and technical efficiency. Despite the short timeline, our team&rsquo;s commitment to clean code, dynamic functionality, and effective collaboration enabled us to create a functional and delightful product. This experience reinforced the importance of structure and code organization in collaborative projects, and ensured that each member&rsquo;s contributions fit suently into the whole.</p>
-        </div>
-      </div>
+    {/* Summary  */}
+    <Section title={conclusion.title} paragraph={conclusion.paragraph}/>
       
 
       <div className="flex justify-between items-center">

ba3ec946eaf09d2b776d8b89f9fa16af1df096d1
Nur Gültekin <92712594+nurgultekin@users.noreply.github.com>
2024-11-27 20:54:40 +0100

    -

--- nur-website/src/assets/components/Hero.jsx
+++ nur-website/src/assets/components/Hero.jsx
@@ -19,7 +19,7 @@ const HeroSection = () => {
     }, []);
 
     return (
-        <div className="flex flex-col justify-center items-center mb-0 pt-8 md:pt-24 px-8 lg:px-24 h-full
+        <div className="flex flex-col justify-center items-center mb-0 pt-8 md:pt-24 px-4 lg:px-24 h-full
 ">
             <div>
                 <h1 className="text-4xl md:text-6xl lg:text-[3.5rem] xl:text-[6rem] text-[#CDE82B] font-titan tracking-wide text-center mb-5 z-[1000] text-stroke">
@@ -28,8 +28,8 @@ const HeroSection = () => {
                 </h1>
             </div>
 
-            <div className="w-[100%] lg:max-w-3xl">
-                <p className="text-white text-[1rem] md:text-[1rem] lg:text-[1.2rem] mx-auto md:ml-12 text-shadow font-semibold text-lg md:text-xl leading-[130%] font-sans text-left">
+            <div className="w-full max-w-lg sm:max-w-xl lg:max-w-3xl">
+                <p className="text-white text-[1rem] md:text-[1rem] lg:text-[1.2rem] mx-auto md:ml-12 text-shadow font-semibold text-lg md:text-xl leading-[130%] font-roboto text-left">
                     I am <span className=" text-[1.2rem] lg:text-[1.6rem] text-[#b4b459] font-titan leading-normal">Nur</span>, (a.k.a. Nora), a front-end software developer from Istanbul, now based in Stockholm. I draw great inspiration from arts, games, and nature. I enjoy applying my creativity and logical thinking to design and develop interactive technologies that are both enjoyable and usable.  
                 </p>
             </div>

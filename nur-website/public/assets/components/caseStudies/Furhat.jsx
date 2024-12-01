50ae6082efaecfff9e01d4d4ce540f341255c9d3
nurgultekin <92712594+nurgultekin@users.noreply.github.com>
2024-12-01 01:32:28 +0100

    -

--- nur-website/public/assets/components/caseStudies/Furhat.jsx
+++ nur-website/public/assets/components/caseStudies/Furhat.jsx
@@ -4,6 +4,7 @@ import '../../../App.css';
 import AnimationDark from "../AnimationDark";
 import Section from "../SectionsLayout";
 import 'tailwindcss/tailwind.css';
+import { Link } from "react-router-dom";
 
 const Furhat = () => {
 
@@ -170,11 +171,11 @@ const designAndDevelopmentProcess = {
         </div>
   
   
-        <a href="/KappnsCatch" className="ml-2">
+        <Link to="/KappnsCatch" className="ml-2">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="size-12">
             <path strokeLinecap="round" strokeLinejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
           </svg>
-        </a>
+        </Link>
       </div>
 
 

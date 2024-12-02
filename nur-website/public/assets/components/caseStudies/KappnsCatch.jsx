62bab14fbc3ea440784902d747d16be7fcecd088
nurgultekin <92712594+nurgultekin@users.noreply.github.com>
2024-12-02 16:40:07 +0100

    KappncCatch redirect to Luxebite

--- nur-website/public/assets/components/caseStudies/KappnsCatch.jsx
+++ nur-website/public/assets/components/caseStudies/KappnsCatch.jsx
@@ -6,6 +6,7 @@ import '../../../App.css';
 import Section from "../SectionsLayout";
 import AnimationDark from "../AnimationDark";
 import 'tailwindcss/tailwind.css';
+import { Link } from "react-router-dom";
 
 const KappnsCatch = () => {
 
@@ -232,11 +233,11 @@ const conclusion = {
           <p className="font-titan text-white text-[14px] m-0">Next Case Study:</p>
           <p className="font-titan text-white text-[18px] m-0">LuxeBite</p>
         </div>
-            <a href="/LuxebiteCaseStudy">
+            <Link to="/LuxebiteCaseStudy">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="size-12">
               <path strokeLinecap="round" strokeLinejoin="round" d="m12.75 15 3-3m0 0-3-3m3 3h-7.5M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
             </svg>
-            </a>
+            </Link>
 
           </div>
 
